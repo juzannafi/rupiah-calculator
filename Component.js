1816cb30cd8faa73c142b936cbfa1a354be37258
juzannafi <juzannafihaifa@gmail.com>
2020-02-08 13:54:46 +0700

    add component

---
+++ Component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, ScrollView, View, Text } from 'react-native';
+
+export default class Component extends React.Component {
+
+  render() {
+    return (
+      <View style={{backgroundColor: 'blue'}}>
+          <Text>
+            Rp100.000
+          </Text>
+        </View>
+    )
+  }
+}
